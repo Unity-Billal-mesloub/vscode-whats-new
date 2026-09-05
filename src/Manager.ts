37d7957b21975b532b46ebc9ea92412a1510d5bf
Billal Mesloub <mesloubi2021@gmail.com>
2026-09-05 21:11:21 +0100

    Remove copyright notice from Manager.ts
    Removed copyright notice from Manager.ts

--- src/Manager.ts
+++ src/Manager.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
-*  Copyright (c) Alessandro Fragnani. All rights reserved.
-*  Licensed under the MIT License. See License.md in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
 
 import * as semver from "semver";
 import * as vscode from "vscode";
